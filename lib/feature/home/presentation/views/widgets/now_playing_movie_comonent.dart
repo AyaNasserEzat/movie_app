b200e16a6146b7bbcb15a5debfee7dfb74354642
unknown <ayanasser1853@gmail.com>
2025-09-16 21:46:55 +0300

    refactor: extract NowPlayingMovieComponent from HomeView

---
+++ lib/feature/home/presentation/views/widgets/now_playing_movie_comonent.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class NowPlayingMovieComponent extends StatelessWidget {
+  const NowPlayingMovieComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 350,
+      child: Stack(
+        children: [
+          // الصورة
+          Image.asset(
+            'assets/images/cartoon_test.jpg',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.fill,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter, // يبدأ من تحت
+                end: Alignment.topCenter, // يخلص فوق
+                colors: [Colors.grey.shade900, Colors.transparent],
+              ),
+            ),
+          ),
+          // now playing
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 10,
+                    children: [
+                      CircleAvatar(radius: 5, backgroundColor: Colors.red),
+                      Text("Now Playing"),
+                    ],
+                  ),
+                ),
+                Text("The Bad Guys"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
