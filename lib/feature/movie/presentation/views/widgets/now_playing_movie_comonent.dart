0cf61f853f9a9a829c0c291f520f58838f9fd500
unknown <ayanasser1853@gmail.com>
2025-09-24 02:33:44 +0300

    feat: add CarouselSlider to NowPlayingMovieComponent

--- lib/feature/movie/presentation/views/widgets/now_playing_movie_comonent.dart
+++ lib/feature/movie/presentation/views/widgets/now_playing_movie_comonent.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app/feature/movie/presentation/mangers/now_playing_movies_cubit/now_playing_movie_state.dart';
@@ -16,74 +18,90 @@ class NowPlayingMovieComponent extends StatelessWidget {
             child: Center(child: CircularProgressIndicator()),
           );
         } else if (state is NowPlayingMovieSuccess) {
-          final movie = state.movies.isNotEmpty ? state.movies.first : null;
+          final movie = state.movies.isNotEmpty ? state.movies : null;
           if (movie == null) {
             return const SizedBox(
               height: 350,
               child: Center(child: Text("No movies available")),
             );
           }
-          return SizedBox(
-            height: 350,
-            child: Stack(
-              children: [
-                // صورة الفيلم
-                Image.network(
-                  "https://image.tmdb.org/t/p/w500${movie.backdropPath ?? movie.posterPath}",
-                  width: double.infinity,
-                  height: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-                // الجريدينت
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                      colors: [Colors.grey.shade900, Colors.transparent],
-                    ),
-                  ),
-                ),
-                // Now Playing + اسم الفيلم
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            CircleAvatar(
-                              radius: 5,
-                              backgroundColor: Colors.red,
-                            ),
-                            SizedBox(width: 8),
-                            Text(
-                              "Now Playing",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Text(
-                        movie.title,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(height: 12),
-                    ],
-                  ),
-                ),
-              ],
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: 400,
+              autoPlay: true,
+              viewportFraction: 1.0,
+
+              scrollDirection: Axis.horizontal,
             ),
+            items:
+                state.movies.map((e) {
+                  return SizedBox(
+                    height: 350,
+                    child: Stack(
+                      children: [
+                        // صورة الفيلم
+                        CachedNetworkImage(
+                          imageUrl:
+                              "https://image.tmdb.org/t/p/w500${e.backdropPath ?? e.posterPath}",
+                          width: double.infinity,
+                          height: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                        // الجريدينت
+                        Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                              colors: [
+                                Colors.grey.shade900,
+                                Colors.transparent,
+                              ],
+                            ),
+                          ),
+                        ),
+                        // Now Playing + اسم الفيلم
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 8.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    CircleAvatar(
+                                      radius: 5,
+                                      backgroundColor: Colors.red,
+                                    ),
+                                    SizedBox(width: 8),
+                                    Text(
+                                      "Now Playing",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Text(
+                                e.title,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const SizedBox(height: 12),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
           );
         } else if (state is NowPlayingMovieFailure) {
           return SizedBox(
