4fa25d3e264b289261e45e9427503ca64d01ba82
unknown <ayanasser1853@gmail.com>
2025-10-05 21:40:20 +0300

    add padding in search text forn field

--- lib/feature/search/presentation/views/widgets/search_text_form_field.dart
+++ lib/feature/search/presentation/views/widgets/search_text_form_field.dart
@@ -8,22 +8,25 @@ class SearchTextFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      onChanged: (value) {
-        BlocProvider.of<SearchCubit>(
-          context,
-        ).searchByName(searchParams: SearchParams(value));
-      },
-      decoration: InputDecoration(
-        hintText: 'search',
-        prefixIcon: Icon(Icons.search, size: 20),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(16),
-          borderSide: BorderSide(color: Colors.white),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(16),
-          borderSide: BorderSide(color: Colors.white),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: TextFormField(
+        onChanged: (value) {
+          BlocProvider.of<SearchCubit>(
+            context,
+          ).searchByName(searchParams: SearchParams(value));
+        },
+        decoration: InputDecoration(
+          hintText: 'search',
+          prefixIcon: Icon(Icons.search, size: 20),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide(color: Colors.white),
+          ),
         ),
       ),
     );
