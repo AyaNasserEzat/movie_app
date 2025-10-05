4692938219cac003eb7a93396b9142ac6212cf31
unknown <ayanasser1853@gmail.com>
2025-10-05 21:59:16 +0300

    add debounce in search text form field

--- lib/feature/search/presentation/manger/seach_cubit/seach_cubit.dart
+++ lib/feature/search/presentation/manger/seach_cubit/seach_cubit.dart
@@ -1,21 +1,34 @@
+import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app/feature/search/domain/use_cases/search_use_case.dart';
 import 'package:movies_app/feature/search/presentation/manger/seach_cubit/search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
   SearchCubit(this.searchUseCase) : super(SearchMovieInitial());
-  SearchUseCase searchUseCase;
+  final SearchUseCase searchUseCase;
 
-  Future<void> searchByName({required SearchParams searchParams}) async {
-    emit(SearchMovieLoading());
-    var result = await searchUseCase.call(searchParams);
-    result.fold(
-      (faiure) {
-        emit(SearchMovieFailure(faiure.errMessage));
-      },
-      (movies) {
-        emit(SearchMovieSuccess(movies));
-      },
-    );
+  Timer? _debounce;
+
+  /// البحث مع تأخير (Debounce)
+  void searchByName({required SearchParams searchParams}) {
+    // لو فيه تايمر شغال نلغيه
+    _debounce?.cancel();
+
+    // نبدأ تايمر جديد (مثلاً بعد 500ms)
+    _debounce = Timer(const Duration(milliseconds: 500), () async {
+      emit(SearchMovieLoading());
+      final result = await searchUseCase.call(searchParams);
+      result.fold(
+        (failure) => emit(SearchMovieFailure(failure.errMessage)),
+        (movies) => emit(SearchMovieSuccess(movies)),
+      );
+    });
+  }
+
+  @override
+  Future<void> close() {
+    // نلغي التايمر لما الكيوبت يتقفل
+    _debounce?.cancel();
+    return super.close();
   }
 }
